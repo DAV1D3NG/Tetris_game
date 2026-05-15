a9598b78edfb322827205addcf665971f43688ad
DAVIDENG <dangelodvd08@gmail.com>
2026-05-15 17:38:35 +0200

    Update main.py
    Fix working directory path for executable compatibility.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os, sys
 from src.game import main
+
+# Set the working directory to the folder where the .exe file is running
+os.chdir(os.path.dirname(sys.argv[0]))
 
 if __name__ == "__main__":
     main()
